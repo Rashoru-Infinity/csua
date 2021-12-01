75b34e352ea2e94d49dda20d90c25153241cb3fe
Rashoru-Infinity <65536toaru@gmail.com>
2021-12-02 01:58:43 +0900

    modified:   .gitignore 	new file:   comp/tests/prog5.cs 	modified:   svm/svm.c

--- svm/svm.c
+++ svm/svm.c
@@ -5,10 +5,11 @@
 #include <sys/fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include "svm.h"
 #include "../memory/MEM.h"
 
-
+/* 渡したポインタから4byte分値を取りだしてintの値を返す 取り出した分だけポインタをずらす */
 static int read_int(uint8_t **p) {
     uint8_t v1 = **p; (*p)++;
     uint8_t v2 = **p; (*p)++;
@@ -18,6 +19,7 @@ static int read_int(uint8_t **p) {
     return v;
 }
 
+/* 渡したポインタから8byte分値を取りだしてdoubleの値を返す 取り出した分だけポインタをずらす */
 static double read_double(uint8_t **p) {
     uint8_t v1 = **p; (*p)++;
     uint8_t v2 = **p; (*p)++;
@@ -35,11 +37,13 @@ static double read_double(uint8_t **p) {
 
 }
 
+/* 渡したポインタから1byte分値を取りだして符号なしの1byte分の値を返す 取り出した分だけポインタをずらす */
 static uint8_t read_byte(uint8_t **p) {
     uint8_t v = **p; (*p)++;
     return v;
 }
 
+/* ファイル先頭8byte(header)の内容をchar型として出力する 読み出した分だけポインタをずらす */
 static void parse_header(uint8_t **p) {
     for(int i = 0; i < 8; ++i) {
         printf("%c ", (char)(**p));
@@ -63,6 +67,7 @@ static void dump(DInfo *info) {
     info->r_index = 0;
 }
 
+/* 命令の名前をバッファに突っ込む(20文字) 余ったら空白文字で埋める */
 static void add_opname(DInfo *info, const char* str) {
     int len = strlen(str);
     strncpy(&info->s_buf[info->s_index], str, len);
@@ -73,16 +78,19 @@ static void add_opname(DInfo *info, const char* str) {
     info->s_buf[info->s_index] = 0;
 }
 
+/* 4文字分空白文字でバッファを埋める */
 static void add_padding(DInfo *info) {
     for(int i = 0; i < 4; ++i, ++info->s_index) info->s_buf[info->s_index] = ' ';    
 }
 
+/* 2byte分の領域を16進にしてバッファに詰める */
 static void add_uint16(DInfo *info, const uint16_t iv) {
     char buf[6] = {};
     sprintf(buf, "%04x", iv);
     strncpy(&info->s_buf[info->s_index], buf, 4);
 }
 
+/* 16進ダンプ用のバッファにopの値を16進で突っ込む */
 static void add_rowcode(DInfo *info, uint8_t op) {
 //    printf("add row %02x\n", op);
     sprintf(&info->row_buf[info->r_index], "%02x", op);
@@ -117,13 +125,16 @@ static void disasm(SVM_VirtualMachine* svm) {
                 exit(1);
             }
         }
+		/* 命令の取るパラメータのバイトコードを16進にしてバッファに突っ込む */
         param_len = strlen(oinfo->parameter);
         for (int j = 0; j < param_len; ++j) {
             switch(oinfo->parameter[j]) {
                 case 'i': {
                     uint8_t uv = *(++p);
                     uint16_t op = (uint16_t)( uv << 8 | *(++p));
+					/* 引数2byte分の16進ダンプをバッファに突っ込む */
                     add_uint16(&dinfo, op);
+					/* 先ほど命令の16進ダンプを突っ込んだバッファに引数2byte分の16進ダンプを突っ込む */
                     add_rowcode(&dinfo, uv);
                     add_rowcode(&dinfo, *p);                     
                     i += 2;
@@ -135,8 +146,9 @@ static void disasm(SVM_VirtualMachine* svm) {
                 }
             }
         }
+		/* 4文字分空白文字でバッファを埋める */
         if (param_len == 0) add_padding(&dinfo);
-        
+        /* 命令と引数とコードの16進ダンプを吐き出す */
         dump(&dinfo);        
     }    
 }
@@ -145,19 +157,25 @@ static void disasm(SVM_VirtualMachine* svm) {
 static void parse(uint8_t* buf, SVM_VirtualMachine* svm) {
     uint8_t* pos = buf;
     parse_header(&pos);
+	/* 定数プール内の定数の個数(4byte領域)を読み出す 読み出しただけポインタをずらす */
     svm->constant_pool_count = read_int(&pos);
     printf("constant_pool_count = %d\n", svm->constant_pool_count);
+	/* 定数プールのメモリを確保する */
     svm->constant_pool = (SVM_Constant*)MEM_malloc(sizeof(SVM_Constant) * svm->constant_pool_count);
     
     uint8_t type;
+	/* 定数の数だけ値を読み出す 読み出しただけポインタをずらす */
     for (int i = 0; i < svm->constant_pool_count; ++i) {
+		/* 1byte領域読んだ時の値でintかdoubleか判定する */
         switch(type = read_byte(&pos)) {
+			/* intは4byte読み出して共用体にintの値を入れる 読み出した分ポインタをずらす */
             case SVM_INT: {
                 int v = read_int(&pos);
                 svm->constant_pool[i].type = SVM_INT;
                 svm->constant_pool[i].u.c_int = v;
                 break;
             }
+			/* doubleは8byte読み出して共用体にdoubleの値を入れる 読み出した分だけポインタをずらす */
             case SVM_DOUBLE: {
                 double dv = read_double(&pos);
                 svm->constant_pool[i].type = SVM_DOUBLE;
@@ -170,13 +188,16 @@ static void parse(uint8_t* buf, SVM_VirtualMachine* svm) {
             }
         }
     }
-    
+    /* 変数の個数を読み出す(4byte) 読み出した分だけポインタをずらす */
     svm->global_variable_count = read_int(&pos);
+	/* 変数の値を保持するためのメモリを確保する */
     svm->global_variables = (SVM_Value*)MEM_malloc(sizeof(SVM_Value) * svm->global_variable_count);
+	/* 変数の型情報を格納するためのメモリを確保する */
     svm->global_variable_types = (uint8_t*)MEM_malloc(sizeof(uint8_t) * svm->global_variable_count);
     printf("global_variable_count = %d\n", svm->global_variable_count);
     for (int i = 0; i < svm->global_variable_count; ++i) {        
 //        uint8_t type = read_byte(&pos);
+		/* 変数の型情報を読み出す 読み出した分だけポインタをずらす */
         svm->global_variable_types[i] = read_byte(&pos);
         switch (svm->global_variable_types[i]) {
             case SVM_INT: {
@@ -192,12 +213,16 @@ static void parse(uint8_t* buf, SVM_VirtualMachine* svm) {
             }
         }
     }
-    
+    /* コードサイズを読み出す 読み出した分だけポインタをずらす */
     svm->code_size = read_int(&pos);
     printf("code_size = %d\n", svm->code_size);
+	/* コード領域のメモリを確保する */
     svm->code = (uint8_t*)MEM_malloc(svm->code_size);
+	/* 現在のポインタの位置がコード領域の開始地点なのでそこからコードサイズ分メモリにコピーする */
     memcpy(svm->code, pos, svm->code_size);
+	/* メモリにコピーした分(code size分)だけポインタをずらす */
     pos += svm->code_size;
+	/* プログラムが使用するスタックサイズを読み出す ポインタをずらす */
     svm->stack_size = read_int(&pos);
     printf("stack_size = %d\n", svm->stack_size);    
     
@@ -206,7 +231,7 @@ static void parse(uint8_t* buf, SVM_VirtualMachine* svm) {
  
 }
 
-
+/* svmのためのメモリを確保して0埋めしてからアドレスを返す */
 static SVM_VirtualMachine* svm_create() {
     SVM_VirtualMachine* svm = (SVM_VirtualMachine*)MEM_malloc(sizeof(SVM_VirtualMachine));
     svm->constant_pool = NULL;
@@ -253,6 +278,7 @@ static void svm_delete(SVM_VirtualMachine* svm) {
     MEM_free(svm);
 }
 
+/* ネイティブ関数のポインタ，関数名，引数の個数を格納する */
 void svm_add_native_function(SVM_VirtualMachine* svm, SVM_NativeFunction native_f, char* name, int arg_count) {
     svm->functions = (SVM_Function*)MEM_realloc(svm->functions, sizeof(SVM_Function) * (svm->function_count+1));
     svm->functions[svm->function_count].f_type = NATIVE_FUNCTION;
@@ -262,11 +288,12 @@ void svm_add_native_function(SVM_VirtualMachine* svm, SVM_NativeFunction native_
     svm->function_count++;    
 }
 
-
+/* プログラムカウンタに対応するバイトコードを1byte返す */
 static uint8_t fetch(SVM_VirtualMachine* svm) {
     return svm->code[svm->pc++];
 }
 
+/* プログラムカウンタに対応するバイトコードを2byte返す */
 static uint16_t fetch2(SVM_VirtualMachine* svm) {
     uint8_t v1 = fetch(svm);
     return (v1 << 8) | fetch(svm);
@@ -307,12 +334,16 @@ static int read_global_i(SVM_VirtualMachine* svm, uint32_t idx) {
 
 
 static void init_svm(SVM_VirtualMachine* svm) {
+	/* スタックを必要なサイズだけ確保する */
     svm->stack = (SVM_Value*)MEM_malloc(sizeof(SVM_Value) * svm->stack_size);
+	/* 型情報を格納する領域を確保する */
     svm->stack_value_type = (uint8_t*)MEM_malloc(sizeof(uint8_t) * svm->stack_size);
+	/* プログラムカウンタを0にする */
     svm->pc = 0;
+	/* スタックポインタを0にする */
     svm->sp = 0;
     
-    
+	/* グローバル変数をすべて0で初期化する */ 
     for (int i = 0; i < svm->global_variable_count; ++i) {
         switch(svm->global_variable_types[i]) {
             case SVM_INT: {
@@ -331,6 +362,7 @@ static void init_svm(SVM_VirtualMachine* svm) {
     }    
 }
 
+/* グローバル変数とスタックの内容を出力する */
 static void show_status(SVM_VirtualMachine* svm) {
     printf("< show SVM status >\n");
     printf("-- global variable ---\n");
@@ -371,18 +403,21 @@ static void show_status(SVM_VirtualMachine* svm) {
     
 }
 
+/* スタックマシンを実行する */
 static void svm_run(SVM_VirtualMachine* svm) {    
     bool running = true;
 
     uint8_t op = 0;
     while(running) {
         switch (op = fetch(svm)) {
+			/* int型定数をconstant poolから読み出してstackにpush */
             case SVM_PUSH_INT: { // push from constant pool
                 uint16_t s_idx = fetch2(svm);
                 int v = read_static_int(svm, s_idx);
                 push_i(svm, v);
                 break;
             }
+			/* int型の値をpopしてglobal変数に書き込む */
             case SVM_POP_STATIC_INT: { // save val to global variable
                 uint16_t s_idx = fetch2(svm); 
                 int iv = pop_i(svm);
@@ -391,6 +426,7 @@ static void svm_run(SVM_VirtualMachine* svm) {
 //                exit(1);
                 break;
             }
+			/* int型のグローバル変数の値をstackにpushする */
             case SVM_PUSH_STATIC_INT: {
                 uint16_t s_idx = fetch2(svm);
                 int iv = read_global_i(svm, s_idx);
@@ -398,17 +434,20 @@ static void svm_run(SVM_VirtualMachine* svm) {
                 push_i(svm, iv);
                 break;
             }
+			/* スタックの値を2つpopして値を足したものをstackにpushし直す */
             case SVM_ADD_INT: {
                 int iv1 = pop_i(svm);
                 int iv2 = pop_i(svm);
                 push_i(svm, (iv1+iv2));
                 break;
             }
+			/* pushする関数のインデックスを取得する */
             case SVM_PUSH_FUNCTION: {
                 uint16_t idx = fetch2(svm);
                 push_i(svm, idx);
                 break;
             }
+			/* 関数を実行する */
             case SVM_INVOKE: {
                 uint16_t f_idx = pop_i(svm);
                 /*
@@ -418,6 +457,7 @@ static void svm_run(SVM_VirtualMachine* svm) {
                 printf("sp = %d\n", svm->sp);
                 printf("arg_count = %d\n", svm->functions[f_idx].arg_count);
                  */
+				/* インデックスに対応する関数を実行する */
                 switch (svm->functions[f_idx].f_type) {
                     case NATIVE_FUNCTION: {
                         SVM_Value val = svm->functions[f_idx].u.n_func(svm, 
@@ -480,7 +520,10 @@ int main(int argc, char *argv[]) {
     
     SVM_VirtualMachine* svm = svm_create();
     struct stat st;
-    stat(argv[file_idx], &st);
+    if (stat(argv[file_idx], &st) < 0) {
+		fprintf(stderr, "%s\n",strerror(errno));
+		exit(errno);
+	}
     uint8_t* buf = (uint8_t*)malloc(st.st_size);
     int fp = open(argv[file_idx], O_RDONLY); 
     read(fp, buf, st.st_size);
